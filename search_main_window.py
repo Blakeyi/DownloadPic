d4809766a5724e5219423663a98c4d7f3865c36b
blake <1152420971@qq.com>
2020-05-02 18:08:41 +0800

    修改标题

--- search_main_window.py
+++ search_main_window.py
@@ -9,13 +9,14 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtGui import QIcon
 import os
 import webbrowser
 
 
 class Ui_MainWindow(QtWidgets.QWidget):
     def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
+        MainWindow.setObjectName("图片下载")
         MainWindow.resize(780, 553)
         self.defaultPath = ''
         self.centralwidget = QtWidgets.QWidget(MainWindow)
@@ -120,7 +121,9 @@ class Ui_MainWindow(QtWidgets.QWidget):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "图片下载"))
+        str = self.cwd+'/favicon.ico'
+        self.setWindowIcon(QIcon(str))
         self.cancle.setText(_translate("MainWindow", "取消下载"))
         self.label.setText(_translate("MainWindow", "网址："))
         self.label_2.setText(_translate("MainWindow", "                        欢迎使用图片下载工具"))
